15b3c07a2a95cc7ca5c27ee80e6e20db7d494581
Suyog Patil <psuyog555@gmail.com>
2022-11-30 13:48:18 +0530

    Completed Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EvenNumber
+{
+    class EvenNumber
+    {
+        public void fEvenNo(ref List<long?> ilist)
+        {
+            foreach (var number in ilist)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number + " is Even");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is Odd");
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            List<long?> lNos = new List<long?>(new long?[10]);
+
+            EvenNumber evenNumber = new();
+
+            try
+            {
+                Console.WriteLine("Enter 10 numbers : \n");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    lNos[i] = Convert.ToInt64(Console.ReadLine());
+                }
+
+                evenNumber.fEvenNo(ref lNos);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
