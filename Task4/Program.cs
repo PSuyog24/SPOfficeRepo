ef6962e291c9436f684a7b6aef29182d8550b116
Suyog Patil <psuyog555@gmail.com>
2022-12-14 16:30:27 +0530

    Completed : Task-4

---
+++ Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task4
+{
+    public class Task
+    {
+        public static void Main(String[] args)
+        {
+            Dog dog = new Dog();
+            Console.WriteLine("Enter your dog's name : ");
+            dog.SetName(Console.ReadLine());
+            Console.WriteLine(dog.GetName());
+            dog.Eat();
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("is Eating");
+        }
+    }
+
+    public abstract class Animal
+    {
+        private string? Name;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string? GetName()
+        {
+            return Name;
+        }
+
+        public abstract void Eat();
+    }
+}
