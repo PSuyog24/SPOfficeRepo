d657fa22c98e8f0042dfa160866eb3fe74d86bd3
Suyog Patil <psuyog555@gmail.com>
2022-12-14 13:01:16 +0530

    Completed : Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Task8
+{
+    class Task
+    {
+        public static void Main(String[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            for(int i=0;i<3;i++)
+            {
+                string? name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                arrayList.Add(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            foreach(Person person in arrayList)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
