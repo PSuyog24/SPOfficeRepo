842b59e090c32e164ccf4cf0daf67ea253b9ef33
Suyog Patil <psuyog555@gmail.com>
2022-12-15 16:05:19 +0530

    Completed : Task-23_API

---
+++ Task23_API/BodyClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Task23_API
+{
+    public class BodyClass
+    {
+        public long? FirstNum { get; set; }
+        public long? SecondNum { get; set;}
+        public long? Result { get; set; }
+
+        public string? Error { get; set; }
+    }
+}
---
+++ Task23_API/Controllers/MathController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task23_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // POST api/<MathController>
+        [HttpPost]
+        [Route("Add")]
+        public BodyClass Add(BodyClass body)
+        {
+            BodyClass bodyClass = new();
+            try
+            {
+                bodyClass.Result = Convert.ToInt64(body.FirstNum) + Convert.ToInt64(body.SecondNum);
+            }
+            catch (Exception ex)
+            {
+                bodyClass.Error = "Error has occured : " + ex.Message;
+            }
+            return bodyClass;
+        }
+
+        [HttpPost]
+        [Route("Multiply")]
+        public BodyClass Multiply(BodyClass body)
+        {
+            BodyClass bodyClass = new();
+            try
+            {
+                bodyClass.Result = Convert.ToInt64(body.FirstNum) * Convert.ToInt64(body.SecondNum);
+            }
+            catch (Exception ex)
+            {
+                bodyClass.Error = "Error has occured : " + ex.Message;
+            }
+            return bodyClass;
+        }
+    }
+}
