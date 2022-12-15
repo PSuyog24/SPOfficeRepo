8743bd966a7ba30f735366f95899be67a1086842
Suyog Patil <psuyog555@gmail.com>
2022-12-15 17:06:01 +0530

    Completed : Task-20

---
+++ Task20/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task20.Models;
+
+namespace Task20.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            FormClass form = new FormClass();
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FormClass form)
+        {
+            CookieOptions nameCookie = new CookieOptions();
+
+            nameCookie.Expires = DateTime.Now.AddMinutes(5);
+
+            Response.Cookies.Append("Username", form.Username, nameCookie);
+
+            Console.WriteLine("Cookie Value : " + Request.Cookies["Username"]);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task20/Models/FormClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Task20.Models
+{
+    public class FormClass
+    {
+        public string? Username { get; set; }
+        public string? Address { get; set; }
+        public DateOnly Date { get; set; }
+        public string? Nationality { get; set; }
+        public string? PrefCountry { get; set; }
+        public string? Skills { get; set;}
+    }
+}
