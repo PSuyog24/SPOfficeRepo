1df543eeb80c66e21a7277d926cce47e1884f1a8
Suyog Patil <psuyog555@gmail.com>
2022-12-15 12:33:57 +0530

    Completed : Task-17

---
+++ Task17/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+using Task17.Models;
+
+namespace Task17.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            DataClass data = new DataClass();
+
+            data.Operations = new List<OperationList>();
+
+            data.Operations.Add(new OperationList { OperationId="1",Operation="Addition"});
+            data.Operations.Add(new OperationList { OperationId="2",Operation="Subtraction"});
+
+            return View(data);
+        }
+        [HttpPost]
+        public string Index(DataClass data)
+        {
+            long result = 0;
+
+            if(data.SelectedOperation == 1)
+            {
+                result = data.FirstNumber + data.SecondNumber;
+            }
+            else if (data.SelectedOperation == 2)
+            {
+                result = data.FirstNumber - data.SecondNumber;
+            }
+
+            return "Result is " + result;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task17/Models/DataClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
+
+namespace Task17.Models
+{
+    public class DataClass
+    {
+        public long FirstNumber { get; set; }
+        public long SecondNumber { get; set; }
+        [Display(Name = "Choose Operation")]
+        public List<OperationList>? Operations { get; set; }
+        public int SelectedOperation { get; set; }
+    }
+
+    public class OperationList
+    {
+        public string? OperationId { get; set; }
+        public string? Operation { get; set; }
+    }
+}
