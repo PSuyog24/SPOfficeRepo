88324c01a416f65af3e0cc014d130b6c96308696
Suyog Patil <psuyog555@gmail.com>
2022-12-15 18:12:57 +0530

    Completed : Task-18

---
+++ Task18/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task18.Models;
+
+namespace Task18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<SelectListItem> GetAllItems()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "Rose", Value = "1" });
+            items.Add(new SelectListItem { Text = "Marigold", Value = "2" });
+            items.Add(new SelectListItem { Text = "Tulip", Value = "3" });
+            return items;
+        }
+
+        public ActionResult Index()
+        {
+
+            var model = new ItemClass
+            {
+                //SelectedItemName = new[] {2},
+                ItemList = GetAllItems()
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Task18/Models/ItemClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task18.Models
+{
+    public class ItemClass
+    {
+        public int[] SelectedItemName { get; set; }
+        public IEnumerable<SelectListItem> ItemList { get; set; }
+    }
+}
