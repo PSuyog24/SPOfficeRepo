930ecbcbb6ffaff8ff5f4729b52535563df17168
Suyog Patil <psuyog555@gmail.com>
2022-12-27 14:58:30 +0530

    Changes to Task-20

--- Task20/Models/FormClass.cs
+++ Task20/Models/FormClass.cs
@@ -1,12 +1,25 @@
-﻿namespace Task20.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task20.Models
 {
     public class FormClass
     {
+        [Required(ErrorMessage ="Username is required")]
         public string? Username { get; set; }
         public string? Address { get; set; }
-        public DateOnly Date { get; set; }
-        public string? Nationality { get; set; }
+        [Required(ErrorMessage ="Please Select Date")]
+        [ValidateDate]
+        public DateTime Date { get; set; }
+        public List<string>? Nationality { get; set; }
         public string? PrefCountry { get; set; }
         public string? Skills { get; set;}
+    }
+
+    public class ValidateDate: ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? date, ValidationContext validationContext)
+        {
+            return ((DateTime)date >= Convert.ToDateTime("1900-01-01") && (DateTime)date <= Convert.ToDateTime("2090-01-01"))? ValidationResult.Success: new ValidationResult("Date should be between 01-01-1900 to 01-01-2090");
+        }
     }
 }
